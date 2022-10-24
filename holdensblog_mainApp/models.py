a720a4d61f96fabbc4593714c7fa407e30e43bbc
Holden Case <caseholden@gmail.com>
2022-10-24 13:33:07 -0400

    readded ckeditor

--- holdensblog_mainApp/models.py
+++ holdensblog_mainApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
-#from ckeditor.fields import RichTextField
+from ckeditor.fields import RichTextField
 from django.conf import settings
 # Create your models here.
 
