64a27b05878919af2fe546ee38c314e18b56f8e5
Holden Case <caseholden@gmail.com>
2022-10-24 01:23:49 -0400

    ckeditor

--- holdensblog/settings.py
+++ holdensblog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles'
     'ckeditor',
     'ckeditor_uploader'
     #Created Apps
